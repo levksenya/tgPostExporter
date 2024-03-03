275d1b2276ea0343b9242b3c8bd658b18a00af9a
Anton Sergunov <setosha@gmail.com>
2024-03-03 15:23:43 +0600

    Black list implemented

--- frontend/src/stores/settings.js
+++ frontend/src/stores/settings.js
@@ -6,6 +6,7 @@ export const useSettingsStore = defineStore('settings', () => {
   const notes = ref([{ before: '', after: '' }])
   const photoNote = ref('')
   const defaultNotes = ref('')
+  const blacklist = ref('')
 
   const response = fetch('http://localhost:8083/settings', {
     method: 'GET'
@@ -18,14 +19,16 @@ export const useSettingsStore = defineStore('settings', () => {
         inputText: storedInputText,
         notes: storedNotes,
         photoNote: storedPhotoNote,
-        defaultNotes: storedDefaultNotes
+        defaultNotes: storedDefaultNotes,
+        blacklist: storedBlacklist,
       }) => {
         inputText.value = storedInputText
         notes.value = storedNotes
         photoNote.value = storedPhotoNote
         defaultNotes.value = storedDefaultNotes
+        blacklist.value = storedBlacklist
       }
     )
 
-  return { inputText, notes, photoNote, defaultNotes }
+  return { inputText, notes, photoNote, defaultNotes, blacklist }
 })
